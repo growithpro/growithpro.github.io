8b6abf7853f066a75a1828b58ffbcfb59df832c3
Naveen Joshi <naveen.joshi.net@gmail.com>
2022-05-03 22:35:03 +0530

    react router added with hash router

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="growithpro.github.io/about" element={<About />} />
       </Routes>
     </div>
   );
