68180b851aeae977dc56d358c5a781e7655c2578
Naveen Joshi <naveen.joshi.net@gmail.com>
2022-05-03 22:24:15 +0530

    react router added with hash router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
+  HashRouter,
+} from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
